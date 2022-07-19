47d17bd7f28a9bd4fbc8a81d750d5f88b0a4e075
Daniel Mason <danmasson0@gmail.com>
2022-07-19 17:06:34 -0400

    Updates

---
+++ precache-manifest.ba231b3a59a2db5fbc147e853e6f2090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d228baffedbfe98700f823bbe571e452",
+    "url": "/grocery-bud-react-hook-practise/index.html"
+  },
+  {
+    "revision": "906ca3edeb2eb3bab9e9",
+    "url": "/grocery-bud-react-hook-practise/static/css/main.a66a8055.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/grocery-bud-react-hook-practise/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocery-bud-react-hook-practise/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "906ca3edeb2eb3bab9e9",
+    "url": "/grocery-bud-react-hook-practise/static/js/main.6b2d49a5.chunk.js"
+  },
+  {
+    "revision": "7110a7c9ca4fdcd5f8cb",
+    "url": "/grocery-bud-react-hook-practise/static/js/runtime-main.4de62647.js"
+  }
+]);
